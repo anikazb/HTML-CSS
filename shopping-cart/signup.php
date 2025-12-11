b4ec31fc3dd7a851688cfab78f2fdb719ca97056
Anika Zaman <sritebrinto5@gmail.com>
2025-12-11 12:23:45 +0600

    ok

---
+++ shopping-cart/signup.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sign Up</title>
+</head>
+<body>
+    <h1 style="text-align:center;">Signup Page</h1>
+
+    <form method="post" action="signup_validate.php" style="text-align:center;">
+        User Name: 
+        <input type="text" name="uname" required>
+        <br><br>
+        Password: 
+        <input type="password" name="pass" required>
+        <br><br>
+        <input type="submit" value="Sign Up">
+    </form>
+
+</body>
+</html>
---
+++ shopping-cart/signup_validate.php
@@ -0,0 +1,32 @@
+<?php
+if (isset($_POST['uname']) && isset($_POST['pass'])) {
+    $uname = $_POST['uname'];
+    $pass = $_POST['pass'];
+
+    try {
+        $con = new PDO("mysql:host=localhost;dbname=sessiondb", "root", "");
+        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Insert new user
+        $stmt = "INSERT INTO users(uname, pass) VALUES('$uname', '$pass')";
+        $con->query($stmt);
+
+        echo "<script>
+                alert('Signup Successful! Please Login.');
+                window.location.assign('login.php');
+              </script>";
+
+    } catch(PDOException $e) {
+        echo "<script>
+                alert('User Already Exists or Database Error!');
+                window.location.assign('signup.php');
+              </script>";
+    }
+
+} else {
+    echo "<script>
+            alert('Fill all fields');
+            window.location.assign('signup.php');
+          </script>";
+}
+?>
